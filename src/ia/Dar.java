e8396b0a3f10e9d458554aaa1ec1065ce95391d0
femio23 <chiscazo.paco@gmail.com>
2017-11-09 09:13:38 +0100

    Modificacions de proba.

--- src/ia/Dar.java
+++ src/ia/Dar.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ia;
 
 /**
@@ -10,5 +5,10 @@ package ia;
  * @author femio23
  */
 public class Dar {
-    
+    public Dar(){
+        
+    }
+    public String toString(){
+    return "Bos dias, cachocarne";
+    }
 }
